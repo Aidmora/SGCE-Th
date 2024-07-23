b608c78ba4fa0322b96150471a6695fb01f25884
Ferkhan <133190285+Ferkhan@users.noreply.github.com>
2024-07-23 02:09:14 -0500

    diseñar ConsultarPagoPendiente

--- SGCE-Th/src/controllerPagos/ConsultarPagoPendiente.java
+++ SGCE-Th/src/controllerPagos/ConsultarPagoPendiente.java
@@ -1,9 +1,39 @@
 package controllerPagos;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class ConsultarPagoPendiente {
 
 
-    public void hacerNada() {
-        System.out.println("no hace nada");
+    @FXML
+    void actionCerrarSesion(ActionEvent event) throws IOException {
+        navigateTo("/fxml/Login.fxml", event);
+    }
+
+    @FXML
+    void actionConsultaPaciente(ActionEvent event) throws IOException {
+        navigateTo("/fxml/ConsultarPaciente.fxml", event);
+    }
+
+    @FXML
+    void actionActualizarPaciente(ActionEvent event) throws IOException {
+        navigateTo("/fxml/ActualizarPaciente.fxml", event);
+    }
+
+    private void navigateTo(String fxmlPath, ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxmlPath)));
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
