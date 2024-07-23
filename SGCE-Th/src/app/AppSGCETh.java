d22d9c31ca18ea24bf1e664afce2f2ccff6ac6ee
Ferkhan <133190285+Ferkhan@users.noreply.github.com>
2024-07-23 03:26:07 -0500

    funcionar menu

--- SGCE-Th/src/app/AppSGCETh.java
+++ SGCE-Th/src/app/AppSGCETh.java
@@ -10,7 +10,7 @@ public class AppSGCETh extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-            Parent root = FXMLLoader.load(getClass().getResource("/fxml/Pagos/ConsultarPagoPendiente.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/Pagos/ActualizarPagoPendiente.fxml"));
             Scene scene = new Scene(root);
 
             stage.setScene(scene);
