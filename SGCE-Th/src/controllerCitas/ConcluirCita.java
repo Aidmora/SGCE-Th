42e1fbe4d4609a99637b0efeadacb78d93e9a759
Ferkhan <133190285+Ferkhan@users.noreply.github.com>
2024-08-05 00:57:01 -0500

    modificar y agregar Citas

--- SGCE-Th/src/controllerCitas/ConcluirCita.java
+++ SGCE-Th/src/controllerCitas/ConcluirCita.java
@@ -11,6 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class ConcluirCita {
     @FXML
@@ -30,9 +31,6 @@ public class ConcluirCita {
 
     @FXML
     private MenuItem btnActualizarPaciente;
-
-    @FXML
-    private Button btnCerrarSesion;
 
     @FXML
     private MenuItem btnConsultaCita;
@@ -70,104 +68,16 @@ public class ConcluirCita {
     @FXML
     private TextField txtNumCedula21;
 
-    @FXML
-    void actionRegistroTratamiento(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Tratamientos/RegTratamiento.fxml");
-    }
-    @FXML
-    void actionConsultarTratamiento(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Tratamientos/ConsultarTratamientos.fxml");
-    }
-    @FXML
-    void actionActualizarTratamiento(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Tratamientos/ActualizarTratamientos.fxml");
-    }
-    @FXML
-    void actionActualizarPaciente(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Pacientes/ActualizarPaciente.fxml");
-    }
-
-    @FXML
-    void actionCerrarSesion(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Login.fxml");
-    }
-
-    @FXML
-    void actionRegistroPaciente(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Pacientes/RegistrarPaciente.fxml");
-    }
-
-    @FXML
-    void actionConsultaPaciente(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Pacientes/ConsultarPaciente.fxml");
-    }
-    @FXML
-    void actionRegistroCita(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Citas/RegistrarCita.fxml");
-    }
-    @FXML
-    void actionRegistroMedico(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Medicos/RegistrarMedico.fxml");
-    }
-    @FXML
-    void actionConsultarMedico(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Medicos/ConsultarMedico.fxml");
-    }
-    @FXML
-    void actionActualizarMedico(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Medicos/ActualizarMedico.fxml");
-    }
-    @FXML
-    void actionConsultarHorarioMedico(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Medicos/ConsHorarioLaboralMedico.fxml");
-    }
-    @FXML
-    void actionConsultarPagoRealizado(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Pagos/ConsultarPagoRealizado.fxml");
-    }
-    @FXML
-    void actionConsultarPagoPendiente(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Pagos/ConsultarPagoPendiente.fxml");
-    }
-    @FXML
-    void actionRegistrarPagoRealizado(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Pagos/RegistrarPagoRealizado.fxml");
-    }
-    @FXML
-    void actionRegistrarPagoPendiente(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Pagos/RegistrarPagoPendiente.fxml");
-    }
-    @FXML
-    void actionActualizarPagoPendiente(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Pagos/ActualizarPagoPendiente.fxml");
-    }
-    @FXML
-    void actionActualizarPagoRealizado(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Pagos/ActualizarPagoRealizado.fxml");
-    }
-
-    @FXML
-    void actionConsultarCita(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Citas/ConsultarCita.fxml");
-    }
-    @FXML
-    void actionActualizarCita(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Citas/ActualizarCita.fxml");
-    }
-    private void navigateTo(String fxmlPath, Stage currentStage) throws IOException {
-        currentStage.hide();
-        Stage newStage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
-        newStage.setScene(new Scene(root));
-        newStage.show();
-    }
-    private void navigateTo(String fxmlPath) throws IOException {
-        Stage currentStage = (Stage) btnCerrarSesion.getScene().getWindow();
-        currentStage.hide();
-        Stage main = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
-        main.setScene(new Scene(root));
-        main.show();
+    private void navigateTo(String fxmlPath) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxmlPath)));
+            Stage stage = (Stage) btnAceptar.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
--- SGCE-Th/src/controllerCitas/ReagendarCitas.java
+++ SGCE-Th/src/controllerCitas/ReagendarCitas.java
@@ -10,14 +10,12 @@ import javafx.scene.control.DatePicker;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class ReagendarCitas {
 
     @FXML
     private Button btnCancelar;
-
-    @FXML
-    private Button btnCerrarSesion;
 
     @FXML
     private Button btnGuardar;
@@ -30,27 +28,20 @@ public class ReagendarCitas {
 
     }
     @FXML
-    void actionCerrarSesion(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Login.fxml", (Stage) btnCerrarSesion.getScene().getWindow());
+    void actionRegresarActualizarCita(ActionEvent event) {
+        navigateTo("/fxml/Citas/ActualizarCita.fxml");
     }
-    @FXML
-    void actionRegresarActualizarCita(ActionEvent event) throws IOException {
-        navigateTo("/fxml/Citas/ActualizarCita.fxml", (Stage) btnCancelar.getScene().getWindow());
-    }
-    private void navigateTo(String fxmlPath) throws IOException {
-        Stage currentStage = (Stage) btnCerrarSesion.getScene().getWindow();
-        currentStage.hide();
-        Stage main = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
-        main.setScene(new Scene(root));
-        main.show();
-    }
-    private void navigateTo(String fxmlPath, Stage currentStage) throws IOException {
-        currentStage.hide();
-        Stage newStage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
-        newStage.setScene(new Scene(root));
-        newStage.show();
+
+    private void navigateTo(String fxmlPath) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxmlPath)));
+            Stage stage = (Stage) btnGuardar.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
